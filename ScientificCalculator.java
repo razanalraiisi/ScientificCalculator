779cf77a50c79cf19d188b87393df7717dd048bf
Razan Al Raiisi <razanraiisi@gmail.com>
2025-07-20 11:16:13 +0400

    Initial project setup for ScientificCalculator

---
+++ ScientificCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class ScientificCalculator {
+    public static void main(String[] args){
+
+    }
+    public static void displayMenu(){
+
+    }
+
+}
